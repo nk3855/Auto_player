d3beb3488eefb9d1932f67e900cbf1d78d825567
nk3855 <Eun@Namkungui-MacBook-Pro.local>
2017-01-24 17:01:59 +0900

    앱 구현을 위한 기본 프로젝트 생성(아무 기능도 들어가지 않은 기본 형태)

---
+++ EmotionPlayer/app/src/main/java/com/example/eun/emotionplayer/SongList.java
@@ -0,0 +1,13 @@
+package com.example.eun.emotionplayer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SongList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_song_list);
+    }
+}
