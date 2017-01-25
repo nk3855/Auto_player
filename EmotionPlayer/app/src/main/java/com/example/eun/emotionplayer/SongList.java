fc98b8924857e7dc1d06322a1561f35813cfc8ae
nk3855 <Eun@Namkungui-MacBook-Pro.local>
2017-01-25 23:27:43 +0900

    인터넷에 나와있는 기본 코드형태. 코드 분석 후 ui를 수정할 예정

--- EmotionPlayer/app/src/main/java/com/example/eun/emotionplayer/SongList.java
+++ EmotionPlayer/app/src/main/java/com/example/eun/emotionplayer/SongList.java
@@ -1,13 +1,61 @@
 package com.example.eun.emotionplayer;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class SongList extends AppCompatActivity {
+
+    private ListView listView;
+    public static ArrayList<MusicDto> list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_song_list);
+        getMusicList();
+        listView = (ListView) findViewById(R.id.songList);
+        MyAdapter adapter = new MyAdapter(this, list);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(SongList.this, PlayMusic.class);
+                intent.putExtra("position", position);
+                intent.putExtra("playlist", list);
+                startActivity(intent);
+            }
+        });
+    }
+
+    public void getMusicList(){
+        list = new ArrayList<>();
+        //가져오고 싶은 컬럼 명을 나열합니다. 음악의 아이디, 앰블럼 아이디, 제목, 아스티스트 정보를 가져옵니다.
+        String[] projection = {MediaStore.Audio.Media._ID,
+                MediaStore.Audio.Media.ALBUM_ID,
+                MediaStore.Audio.Media.TITLE,
+                MediaStore.Audio.Media.ARTIST
+        };
+
+        Cursor cursor = getContentResolver().query(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
+                projection, null, null, null);
+
+        while(cursor.moveToNext()){
+            MusicDto musicDto = new MusicDto();
+            musicDto.setId(cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media._ID)));
+            musicDto.setAlbumId(cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.ALBUM_ID)));
+            musicDto.setTitle(cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.TITLE)));
+            musicDto.setArtist(cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.ARTIST)));
+            list.add(musicDto);
+        }
+        cursor.close();
     }
 }
